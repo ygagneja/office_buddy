321000ad9f3279e3931d76dc0d12e1c4c6efa931
Yuvraj <yuvrajg@itk.ac.in>
2019-07-02 22:25:15 +0530

    done

--- index.js
+++ index.js
@@ -93,6 +93,29 @@ app.post("/loginBoss", passport.authenticate('Boss', {failureRedirect: "/"}),fun
 	res.redirect("/boss/" + req.user.username);
 });
 
+app.post("/changePassword", function(req,res){
+	if(req.body.newpassword != req.body.retypepassword){
+		res.redirect("/employees/" + req.user.username + "/profile/changePassword");
+	}
+	else if(req.body.newpassword == req.body.retypepassword){
+		mongo.connect(url, function(err, db){
+			var dbo = db.db("LoginDetails");
+			dbo.collection("Employees").findOne({username: req.user.username}, function(err, doc){
+				if(err) throw err;
+				if(doc.password == req.body.oldpassword){
+					var myQuery = {username: doc.username};
+					dbo.collection("Employees").remove(myQuery);
+					dbo.collection("Employees").insertOne({name: req.user.name, username: req.user.username, password: req.body.newpassword},function(err,result){});
+					res.redirect("/employees/" + req.user.username + "/profile");
+				}
+				else{
+					res.redirect("/employees/" + req.user.username + "/profile/changePassword");
+				}
+			});	
+		});
+	}
+});
+
 app.use(function(req,res,next) {
   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   next();
@@ -173,6 +196,28 @@ app.use(function(req,res,next) {
 				EmployeesArr = [];
 			});
 		});
+	}
+	else{
+		res.sendFile("/template/error.html",{root:__dirname});	
+	}
+});
+
+app.use(function(req,res,next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+})
+.get("/employees/:employee/profile/changePassword", function(req,res){
+	if(typeof req.session.passport === "undefined"){
+		res.sendFile("/template/error.html",{root:__dirname});
+	}
+	else if(typeof req.session.passport.user === "undefined"){
+		res.sendFile("/template/error.html",{root:__dirname});	
+	}
+	else if(req.session.passport.user.username != req.params.employee){
+		res.sendFile("/template/error.html",{root:__dirname});
+	}
+	else if(req.session.passport.user.username == req.params.employee && typeof req.session.passport.user.boss === "undefined"){
+		res.sendFile("template/changepass.html",{root:__dirname});
 	}
 	else{
 		res.sendFile("/template/error.html",{root:__dirname});	
